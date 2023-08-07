0e4827555c5c97f2f8e2f65b2228d89c3f3bce26
Miguel Mariano da Silva <miguelmsdev@gmail.com>
2023-08-07 13:03:01 -0300

    Add initial page

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <h1>Finding you was the second best event in my life, the best one is the present as long as we're both partners and we believe 100% we're soul mates, this is a simple message and also a result of my studies, which I didn't care about before I met you, you made me strive to be the person I've always wanted to become, and although there's still a long way to go, you've inspired me to come miles away from where I was. Thank you, with all we've been through I wouldn't dare to say God didn't have our back during the entire time, and I'm sure that as long as we respect ourselves, Him, and others, we're still gonna be blessed with healh, wisdom, and happiness by him. Let's be strong and always help each other in their journey, because we're two separate beings that together form one completely new one, with a new journey. I truly love you, Beatriz. Thank you for loving me for who I am. I'll never let you down.</h1>
+}
+
+export default Home;
+
