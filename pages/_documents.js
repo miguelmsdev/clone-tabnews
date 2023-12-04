3788bdb4a9b05adc8dacba765c9c338eb3c29705
Miguel Mariano da Silva <miguelmsdev@gmail.com>
2023-12-04 11:43:20 -0300

    Add scripts `lint:check` and `lint:fix`

--- pages/_documents.js
+++ pages/_documents.js
@@ -1,11 +1,14 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
+          />
           {/* Other head elements can go here */}
         </Head>
         <body>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 function Home() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', padding: '20px', alignItems: 'center', background: "#730045", borderRadius: '40px' }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: "20px",
+        alignItems: "center",
+        background: "#730045",
+        borderRadius: "40px",
+      }}
+    >
       <style>
         {`
           .responsive-image {
@@ -22,23 +31,50 @@ function Home() {
           }
         `}
       </style>
-      <div style={{ padding: '10px 1px' }}> {/* Padding around the image */}
+      <div style={{ padding: "10px 1px" }}>
+        {" "}
+        {/* Padding around the image */}
         <img
           className="responsive-image"
           src="/images/20230729_160754.jpg"
           alt="Two Lovers"
-          style={{ display: 'block', margin: 'auto', height: 'auto', borderRadius: '10px' }}
+          style={{
+            display: "block",
+            margin: "auto",
+            height: "auto",
+            borderRadius: "10px",
+          }}
         />
       </div>
-      <div style={{ padding: '10px 1px' }}> {/* Padding around the text */}
-        <h1 className="message" style={{ color: "#ffffff", textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}> {/* Font size set here */}
-          Finding you was the second best event in my life, the best one is the present as long as we're both partners and we believe 100% we're soul mates, this is a simple message and also a result of my studies, which I didn't care about before I met you, you made me strive to be the person I've always wanted to become, and although there's still a long way to go, you've inspired me to come miles away from where I was.
+      <div style={{ padding: "10px 1px" }}>
+        {" "}
+        {/* Padding around the text */}
+        <h1
+          className="message"
+          style={{
+            color: "#ffffff",
+            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+          }}
+        >
+          {" "}
+          {/* Font size set here */}
+          Finding you was the second best event in my life, the best one is the
+          present as long as we're both partners and we believe 100% we're soul
+          mates, this is a simple message and also a result of my studies, which
+          I didn't care about before I met you, you made me strive to be the
+          person I've always wanted to become, and although there's still a long
+          way to go, you've inspired me to come miles away from where I was.
           <br /> {/* Line break here */}
           <br /> {/* Line break here */}
-          Thank you, with all we've been through I wouldn't dare to say God didn't have our back during the entire time, and I'm sure that as long as we respect ourselves, Him, and others, we're still gonna be blessed with health, wisdom, and happiness by him.
+          Thank you, with all we've been through I wouldn't dare to say God
+          didn't have our back during the entire time, and I'm sure that as long
+          as we respect ourselves, Him, and others, we're still gonna be blessed
+          with health, wisdom, and happiness by him.
           <br /> {/* Line break here */}
           <br /> {/* Line break here */}
-          Let's be strong and always help each other in their journey, because we're two separate beings that together form one completely new one, with a new journey.
+          Let's be strong and always help each other in their journey, because
+          we're two separate beings that together form one completely new one,
+          with a new journey.
           <br /> {/* Line break here */}
           <br /> {/* Line break here */}
           I truly love you, Beatriz.
